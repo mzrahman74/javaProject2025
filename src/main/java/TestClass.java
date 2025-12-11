47e818d6824a8fc084cff7039ceb3e18acacddb3
Mohammad Ziaur Rahman <mohammadziaurrahman@Mohammads-iMac.local>
2025-12-11 09:44:36 -0600

    updated

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,10 @@
+public class TestClass {
+
+    public static void main(String[] args){
+        String actual = "PASS";
+        String expected = new String("PASS");
+        System.out.println("PASS");
+        System.out.println(actual.equals(expected));
+
+    }
+}
---
+++ src/main/java/org/example/collectCall.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class collectCall {
+    public static void main(String[] args){
+        HashMap<String, String> names = new HashMap<>();
+
+        names.put("Bangladesh", "Dhaka");
+        names.put("USA", "Austin");
+
+        System.out.println(names);
+    }
+}
