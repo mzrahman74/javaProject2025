fc007521f300e2ab02ae2c88774e2871d4c8d79a
Mohammad Ziaur Rahman <mzrahman74@gmail.com>
2025-12-23 16:39:44 -0600

    updated

--- src/main/java/org/example/numberArray.java
+++ src/main/java/org/example/numberArray.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.OptionalInt;
 
+import static java.util.Arrays.stream;
+
 public class numberArray {
     public static void method_01() {
         int[] myArray = { 4, 5, 10, 11};
@@ -38,7 +40,7 @@ public class numberArray {
     }
     public static void method_04() {
         int [] numbers = {100, 2000, 300, 400, 50, 20, 10};
-    Arrays.stream(numbers).sorted().forEach(System.out::println);
+    stream(numbers).sorted().forEach(System.out::println);
     }
     public static void method_05() {
         int[] ages = {4, 13, 15, 42, 51};
@@ -85,12 +87,12 @@ public class numberArray {
   }
   public static void method_09() {
         int [] arr = { 10, 12, 11, 5 , 6};
-        int secondHighest = Arrays.stream(arr).boxed().sorted(Comparator.reverseOrder()).skip(1).findFirst().get();
+        int secondHighest = stream(arr).boxed().sorted(Comparator.reverseOrder()).skip(1).findFirst().get();
     System.out.println(secondHighest);
   }
   public static void method_10() {
         int[] numbers = {10, 5, 12, 7, 2 };
-        OptionalInt lowestNumberOptional = Arrays.stream(numbers).min();
+        OptionalInt lowestNumberOptional = stream(numbers).min();
         if(lowestNumberOptional.isPresent()) {
             int lowestNumber = lowestNumberOptional.getAsInt();
       System.out.println("The lowest number is: " + lowestNumber);
@@ -99,11 +101,11 @@ public class numberArray {
         }
   }
   public static void main(String[] args){
-      method_15();
+      method_17();
   }
   public static void method_11() {
         int [] numbers = {500, 600, 700, 10, 20, 1, 2};
-    Arrays.stream(numbers).sorted().forEach(s -> System.out.println(s));
+    stream(numbers).sorted().forEach(s -> System.out.println(s));
   }
   public static void method_12() {
        String str = "Yusuf";
@@ -152,4 +154,22 @@ public class numberArray {
 
 
   }
+
+  public static void method_16() {
+        String name = "Austin";
+        char [] nameArray = name.toCharArray();
+        for (char j : nameArray) {
+            System.out.println(j);
+        }
+        for (int j=0; j<nameArray.length; j++) {
+            System.out.println(j + ":" + nameArray[j]);
+        }
+  }
+
+  public static void method_17() {
+        int [] numbers = { 100, 300, 400, 0, 4, 500, 600};
+      int second =  stream(numbers).boxed().sorted().skip(1).findFirst().get();
+      System.out.println(second);
+  }
+
 }
