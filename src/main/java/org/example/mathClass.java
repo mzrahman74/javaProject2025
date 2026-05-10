194ad540f9be167b223c90dfb422336492ea1182
Mohammad Rahman <mzrahman74@gmail.com>
2026-05-10 11:32:39 -0500

    created method_03 for random names

--- src/main/java/org/example/mathClass.java
+++ src/main/java/org/example/mathClass.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class mathClass {
     public static void method_01() {
     System.out.println(Math.max(5, 10));
@@ -15,5 +17,16 @@ public class mathClass {
     int index = (int) (Math.random() * colors.length);
         System.out.println("Random number: " + num);
     System.out.println("Random color: " + colors[index]);
+    }
+
+  public static void method_03() {
+    int number = (int) (Math.random() * 4 + 1);
+    String[] names = {"Mohammad", "Nusrat", "Naushin", "Nuhaa", "Yusuf"};
+    int index = (int) (Math.random() * names.length);
+    System.out.println("Random number: " + number);
+    System.out.println("Random names: " + names[index]);
+    }
+    public static void main(String[] args){
+        method_03();
     }
 }
